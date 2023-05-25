221e42ae6c800a205f89a8a116186de4d8699dbd
Vuk <127890473+vukpop@users.noreply.github.com>
2023-05-25 20:06:55 +0200

    Add files via upload
    I changed text because old text was bad in the section services.

---
+++ house radovic-website/main.js
@@ -0,0 +1,9 @@
+
+// Links
+const openInstagram = () => {
+    window.open("https://www.instagram.com/kucaradovic/");
+};
+
+const openBooking = () => {
+    window.open("https://www.booking.com/hotel/me/kuca-radovic.sr.html");
+};
