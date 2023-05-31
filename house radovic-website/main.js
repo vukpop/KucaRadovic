ff9f9cc3ee38b52fe5b7697db8d0ad474fdb5f17
astral <117725332+astral0002@users.noreply.github.com>
2023-05-31 09:55:07 +0200

    Undid one of the last commits
    One of the last commits was an error, this is a fix.

--- house radovic-website/main.js
+++
@@ -1,9 +0,0 @@
-
-// Links
-const openInstagram = () => {
-    window.open("https://www.instagram.com/kucaradovic/");
-};
-
-const openBooking = () => {
-    window.open("https://www.booking.com/hotel/me/kuca-radovic.sr.html");
-};
